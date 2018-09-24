61647dd76d14eeb56be8767aef70bb23ddb60357
Fahmi Saman <34700176+fahmisaman@users.noreply.github.com>
2018-09-24 10:52:48 +0800

    Cobaan

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+<!--End of Zendesk Chat Script-->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DATA CENTER || POLTEKKES KEMENKES GORONTALO</title>
+        <meta name="description" content="Code & Template By Fahmi Saman" />
+	    <meta name="keywords" content="SIPENMARU, POLTEKKES KEMENKES GORONTALO, Gorontalo, sistem informasi pendaftaran mahasiswa baru" />
+	    <meta name="author" content="poltekkesgorontalo.ac.id" />
+	    <meta property="og:image" content="http://datacenter.poltekkesgorontalo.ac.id/sipenmaru/img/logo.png" />
+
+        <!-- Sweat alert cantik -->
+        <link rel="stylesheet" type="text/css" href="lib/sweet-alert.css">      
+	    <script src="lib/sweet-alert.min.js"></script>
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="assets/css/font-awesome.min.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script> -->
+
+        <script language="javascript">
+	        var user="";
+	        var msg="- datacenter || POLTEKKES KEMENKES GORONTALO  ";
+	        var speed=150;
+	        var left="";
+	        var right="";
+	        function scroll_title() {
+	            document.title=left+msg+right;
+	            msg=msg.substring(1,msg.length)+msg.charAt(0);
+	            setTimeout("scroll_title()",speed);
+	        }
+	        scroll_title();
+
+	     </script>
+
+
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="sipenmaru/img/logo2.png">
+        <script type="text/javascript">
+            //<![CDATA[
+            // Adblock Detected
+            window.onload=function(){setTimeout(function(){var e=document.querySelector("ins.adsbygoogle");e&&0==e.innerHTML.replace(/\s/g,"").length&&(e.style.cssText="color:rgba(0,0,0,0.5);font-weight:700;font-size:110%;padding:20px;margin:auto;text-align:center;text-decoration:none;display:block!important;background:rgba(0,0,0,0.02);border:1px solid rgba(0,0,0,0.08)",e.innerHTML="Tampaknya Anda memblokir iklan Google AdSense di browser Anda")},2e3)};
+            //]]>
+
+        </script>
+</head>
+<body style="background:gray;opacity:0.99;background-image:url(edge_data_centers1.jpg);">
+    <center style='color:white;'><img src="sipenmaru/img/logo.png"><h1>Data Center<br />Politeknik Kesehatan Kemenkes Gorontalo</h1></center>
+</body>
+
+</html>
